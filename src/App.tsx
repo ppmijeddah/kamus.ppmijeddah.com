3cda8f0476e874cb29e236be10e56336ca95aaf4
Muhammad Adnan Rizqullah <madnanrizqullah@gmail.com>
2025-02-09 16:37:48 +0300

    fix: remove search bar transperency

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
             </span>
           </h1>
         </div>
-        <div className="bg-transparent rounded-lg shadow-lg p-6 mb-6 sticky top-0">
+        <div className="rounded-lg shadow-lg p-6 mb-6 sticky top-0">
           <div className="relative">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
             <input
