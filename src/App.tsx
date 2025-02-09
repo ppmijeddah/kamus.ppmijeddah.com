f858404423f751f9a0b528c196db4114e0de4324
Muhammad Adnan Rizqullah <madnanrizqullah@gmail.com>
2025-02-09 15:26:43 +0300

    feat: improve search bar with sticky and transparancy

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
             </span>
           </h1>
         </div>
-        <div className="bg-white rounded-lg shadow-lg p-6 mb-6">
+        <div className="bg-transparent rounded-lg shadow-lg p-6 mb-6 sticky top-0">
           <div className="relative">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
             <input
