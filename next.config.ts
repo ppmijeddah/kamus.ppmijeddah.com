945e63ab005c38a6a2b89196c2d6e6b8104eec89
Muhammad Adnan Rizqullah <madnanrizqullah@gmail.com>
2025-05-18 04:43:49 +0300

    ci: move to netlify

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,9 +8,5 @@ const nextConfig: NextConfig = {
     },
   },
 };
-
-if (process.env.NODE_ENV === "development") {
-  setupDevPlatform().catch(console.error);
-}
 
 export default nextConfig;
