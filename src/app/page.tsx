3455616e3eda44b666c2a02fff12c4550672379a
Muhammad Adnan Rizqullah <madnanrizqullah@gmail.com>
2025-05-25 08:21:10 +0300

    finalize data form source book

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import DictionaryPageContainer from "@/modules/dictionary/components/dictionary-page-container";
-import { getAllCategories } from "@/services/db/categories";
 import { fetchDictionaryEntries } from "@/modules/dictionary/api/dictionary"; // Import the function
 import { DictionaryEntry } from "@/domain/dictionary"; // Import the type
+import { getAllCategories } from "@/modules/search-filter/api/categories";
 
 export default async function DictionaryPage() {
   const categories = await getAllCategories();
--- src/modules/search-filter/api/categories/index.ts
+++ src/modules/search-filter/api/categories/index.ts
@@ -2,5 +2,5 @@ import { Category } from "@/domain/dictionary";
 import { apiClient } from "@/services/api/client";
 
 export async function getAllCategories(): Promise<Category[]> {
-  return await apiClient.get<Category[]>("/api/categories");
+  return await apiClient.get<Category[]>("/api/categories", "force-cache");
 }
